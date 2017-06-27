a1b846c4533caf7713c71fa253242a6ef5b85d6b
galan <galan@MacBook-Pro-de-galan.local>
2017-06-27 09:26:19 +0200

    Initial import

---
+++ src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        Test ckeditor
+      </q-toolbar-title>
+    </div>
+
+    <div class="layout-view">
+      <ckeditor v-model="content" :config="config"></ckeditor>
+      <modal-component></modal-component>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import Ckeditor from 'vue-ckeditor2'
+import ModalComponent from 'components/ModalComponent'
+
+export default {
+  components: {
+    Ckeditor,
+    ModalComponent
+  },
+  data () {
+    return {
+      content: '',
+      config: {
+        toolbar: [
+          [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ]
+        ],
+        height: 300
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <q-modal ref="basicModal">
+      <h4>Basic Modal</h4>
+      <ckeditor v-model="content" :config="config"></ckeditor>
+      <button class="primary" @click="$refs.basicModal.close()">Close</button>
+    </q-modal>
+    <button @click="openModal">Open modal</button>
+  </div>
+</template>
+
+<script>
+  import Ckeditor from 'vue-ckeditor2'
+
+  export default {
+    name: 'ModalComponent',
+    components: {
+      Ckeditor
+    },
+    data () {
+      return {
+        config: {
+          toolbar: [
+            [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ]
+          ],
+          height: 300
+        }
+      }
+    },
+    methods: {
+      openModal () {
+        this.$refs.basicModal.open()
+      }
+    }
+  }
+</script>
